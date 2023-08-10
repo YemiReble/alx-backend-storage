73e33a904cde6c99fd24353a9360118b5cfc04db
Sulaimon <abodunrinsulaimon1@gmail.com>
2023-08-10 15:26:30 +0000

    This sql file Creates Users Table

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,11 @@
+-- This sql file creates a 'users' table using the below parameter
+-- id, integer, never null, auto increment and primary key
+-- email, string (255 characters), never null and unique
+-- name, string (255 characters)
+
+-- creating database first
+-- CREATE DATABASE IF NOT EXISTS holberton;
+
+CREATE TABLE IF NOT EXISTS users (id INT AUTO_INCREMENT PRIMARY KEY,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255) NOT NULL);
