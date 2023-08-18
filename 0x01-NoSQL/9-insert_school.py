9639b5c682150d48c6cc85def1a25c9b9e5097d3
Sulaimon <abodunrinsulaimon1@gmail.com>
2023-08-18 07:14:35 -0700

    Insert New Document In A Collection

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" Python script that create a function
+that inserts a new document in a collection
+based on kwargs
+"""
+
+
+import pymongo
+
+
+def insert_school(mongo_collection, **kwargs):
+    """ A function that inserts a new document in a
+    Database colletion
+    """
+    return mongo_collection.insert_one(kwargs)
